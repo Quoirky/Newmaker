8eac09496c5991c4cd8ffb9c0cea49617fe3a5e4
Maxwell <88914229+Quoirky@users.noreply.github.com>
2024-01-04 18:32:42 +0100

    Add files via upload

--- javascript/mobileWarning.js
+++ javascript/mobileWarning.js
@@ -1,24 +1,11 @@
-// Function to check if the device is a mobile device
-function isMobileDevice() {
-    return window.matchMedia('(max-width: 768px)').matches;
-  }
-  
-  // Function to show the warning message
-  function showMobileWarning() {
-    var mobileWarning = document.getElementById('mobileWarning');
-    if (isMobileDevice()) {
-      mobileWarning.classList.remove('hidden');
+document.addEventListener('DOMContentLoaded', function() {
+    var userAgent = navigator.userAgent;
+
+    var isIOS = /iPad|iPhone|iPod/.test(userAgent);
+    var isAndroid = /Android/.test(userAgent);
+
+    if (isIOS || isAndroid) {
+        var mobileWarning = document.getElementById('mobileWarning');
+        mobileWarning.classList.add('visible');
     }
-  }
-  
-  // Function to proceed to the website
-  function proceedToWebsite() {
-    var mobileWarning = document.getElementById('mobileWarning');
-    mobileWarning.classList.add('hidden');
-  }
-  
-  // Run the detection and show warning if necessary
-  document.addEventListener('DOMContentLoaded', function () {
-    showMobileWarning();
-  });
-  
+});
